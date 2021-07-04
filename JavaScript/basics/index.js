ec8fdb4630027b5ae1b94d1a1a66dd1f9be758cb
RomanDeboi <granataaa2495@gmail.com>
2021-07-04 22:41:39 +0300

    cxcxxc

--- JavaScript/basics/index.js
+++ JavaScript/basics/index.js
@@ -1,8 +1,6 @@
-function Accumulator(startingValue) {
-    this.value = startingValue;
-    this.read = function() {
-        this.value += +prompt('Enter value!', 0);
-    };
+for (let i = 1; i <= 5; i++) {
+  console.log(`\n=> ${i} <=\n\n`);
+  for (let j = 1; j <= 10; j++) {
+    console.log(i + ' x ' + j + ' = ' + i * j);
+  }
 }
-
-let accumulator = new Accumulator(5);
