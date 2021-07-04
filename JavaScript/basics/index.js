fe9ee7b9e21b7bde76da0de5caed33fd8f530792
RomanDeboi <granataaa2495@gmail.com>
2021-07-04 23:53:22 +0300

    l 4 t h 6 v 1

--- JavaScript/basics/index.js
+++ JavaScript/basics/index.js
@@ -1,7 +1,13 @@
 let sum = 0;
 
-for (let i = 0; i <= 10e6; i++) {
+for (let i = 0; i < 1000; i++) {
   sum += i;
 }
 
-console.log(sum);
+let integer = 0;
+let remainder = 0;
+
+integer = Math.trunc(sum / 1234);
+remainder = sum % 1234;
+
+console.log(integer > remainder);
