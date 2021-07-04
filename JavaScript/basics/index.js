71083cc2d572d5312f1a36e60235dc78a16ae004
RomanDeboi <granataaa2495@gmail.com>
2021-07-04 23:42:02 +0300

    l 4 t h 5 v 1

--- JavaScript/basics/index.js
+++ JavaScript/basics/index.js
@@ -1,13 +1,7 @@
 let sum = 0;
-for (let i = 0; i <= 1000; i++) {
-  if (i % 2 === 1) {
-    console.log('Found');
-    sum += i;
-  }
+
+for (let i = 0; i <= 10e6; i++) {
+  sum += i;
 }
 
-if (sum * 5 > 5000) {
-  console.log('Bigger');
-} else {
-  console.log('Smaller or equal');
-}
+console.log(sum);
