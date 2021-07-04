aca7ff243ad337b7f79367d2ac47f005da4dcc77
RomanDeboi <granataaa2495@gmail.com>
2021-07-04 22:46:41 +0300

    vcvcv

--- JavaScript/basics/index.js
+++ JavaScript/basics/index.js
@@ -1,6 +1,8 @@
-for (let i = 1; i <= 5; i++) {
-  console.log(`\n=> ${i} <=\n\n`);
-  for (let j = 1; j <= 10; j++) {
-    console.log(i + ' x ' + j + ' = ' + i * j);
-  }
+const n = 50;
+let sum = '';
+
+for (let i = 1; i <= n; i++) {
+  sum += String(i);
 }
+
+console.log(sum);
