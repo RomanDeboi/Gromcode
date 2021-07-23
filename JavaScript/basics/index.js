ca09a463ab826cb5b8e15649cf9c01094c2b8c98
RomanDeboi <granataaa2495@gmail.com>
2021-07-23 22:59:09 +0300

    vccv

--- JavaScript/basics/index.js
+++ JavaScript/basics/index.js
@@ -1,13 +1,12 @@
-let sum = 0;
+const superRound = (fractionalNum, numberDigitsAfterDot) => {
+  const coefForRounding = 10 ** numberDigitsAfterDot;
+  return [].concat(
+    Math.floor(fractionalNum * coefForRounding) / coefForRounding,
+    Math.round(fractionalNum * coefForRounding) / coefForRounding,
+    Math.ceil(fractionalNum * coefForRounding) / coefForRounding,
+    Math.trunc(fractionalNum * coefForRounding) / coefForRounding,
+    Number(fractionalNum.toFixed(numberDigitsAfterDot))
+  );
+};
 
-for (let i = 0; i < 1000; i++) {
-  sum += i;
-}
-
-let integer = 0;
-let remainder = 0;
-
-integer = Math.trunc(sum / 1234);
-remainder = sum % 1234;
-
-console.log(integer > remainder);
+console.log(superRound(-1.455454, 3));
