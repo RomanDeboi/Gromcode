71fa1d154a801f1031c6d7d6bf37ca74eea5eea2
RomanDeboi <granataaa2495@gmail.com>
2021-07-28 12:51:16 +0300

    Lesson 13 Home task 1

--- JavaScript/lesson_13/home_task_1/getMinSquaredNumber.test.js
+++ JavaScript/lesson_13/home_task_1/getMinSquaredNumber.test.js
@@ -2,10 +2,10 @@ import { getMinSquaredNumber } from './getMinSquaredNumber.js';
 
 describe('Testing of getMinSquaredNumber function', () => {
   test('If taken empty array then returns null', () => {
-    expect(getMinSquaredNumber([])).toEqual(null);
+    expect(getMinSquaredNumber([])).toBeNull();
   });
   test('If taken string then returns null', () => {
-    expect(getMinSquaredNumber('abc')).toEqual(null);
+    expect(getMinSquaredNumber('abc')).toBeNull();
   });
   test('If taken array then returns squared smallest absolute number', () => {
     expect(getMinSquaredNumber([-2, 45, 2.1, 5])).toEqual(4);
